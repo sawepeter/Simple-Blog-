7a1b664db4669f3a9bc860bfab37574dfcd2065e
sawepeter <sawepeter6@gmail.com>
2023-03-22 04:37:40 +0300

    React js crash course done

--- src/Blogdetails.js
+++ src/Blogdetails.js
@@ -1,9 +1,18 @@
-import { useParams } from "react-router-dom"
+import { useHistory, useParams } from "react-router-dom"
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const { id } = useParams();
     const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/'  + id);
+    const history = useHistory();
+
+    const handleClick = () => {
+        fetch('http://localhost:8000/blogs/' + blog.id, {
+            method: 'DELETE'
+        }).then(() => {
+            history.push('/');
+        })
+    }
 
 
     return (
@@ -15,6 +24,7 @@ const BlogDetails = () => {
                     <h2>{ blog.title } </h2>
                     <div>{ blog.body }</div>
                     <p><strong> Written by { blog.author }</strong></p>
+                    <button onClick={handleClick}>delete</button>
                 </article>
 
            )}
--- src/Create.js
+++ src/Create.js
@@ -1,14 +1,34 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
 
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('');
+    const history = useHistory();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const blog = {title, body, author};
+
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log('new blog added');
+           //history.go(-1);
+           history.push('/');
+        })
+
+        
+    }
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Blog title:</label>
                 <input type="text" 
                 required 
@@ -25,14 +45,14 @@ const Create = () => {
                 <select
                     value={author}
                     onChange={(e) => setAuthor(e.target.value)}>
+
                     <option value="Kiogothe">Kiogothe</option>
                     <option value="Kimondio">Kimondio</option>
                 </select>
 
-                <button>Add Blog</button>
-                <p> {title}</p>
-                <p>{ body }</p>
-
+             <button>Add Blog</button> 
+               
+                
             </form>
 
     
